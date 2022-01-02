c3f572ad25ae74e67d2d47b4a7dafdd326b3b92c
valentyn669 <ownage669@gmail.com>
2022-01-02 19:56:07 +0200

    added a responsive mobile navbar and implemented  a temp db

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import "./App.module.scss";
 import Main from "./components/Main";
 import VinylContainer from "./components/Vinyl-Container";
+import { artistsDatabase } from "./database";
 const App = () => {
   const elRef = useRef<HTMLDivElement>(null);
   const executeScroll = () => {
@@ -12,7 +13,7 @@ const App = () => {
     <>
       <Navbar toElement={executeScroll} />
       <Main />
-      <VinylContainer />
+      <VinylContainer artistsDatabase={artistsDatabase} />
       <div ref={elRef} style={{ height: "1000px" }}>
         element
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,48 @@
 import classes from "./Navbar.module.scss";
 import logo from "../img/logo.png";
+import { ReactComponent as Threelines } from "../img/threelines.svg";
+import { useEffect, useState } from "react";
 interface NavbarProps {
   toElement: () => void;
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ toElement }) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
-    <nav className={classes.navbar}>
-      <img src={logo} alt="Music logo" />
-      <ul className={classes.menuList}>
-        <li onClick={toElement}>Blog</li>
-        <li>About</li>
-        <li>Something</li>
-        <li>Contact</li>
-        <li>Order</li>
-      </ul>
-    </nav>
+    <>
+      <nav className={classes.navbar}>
+        <img src={logo} alt="Music logo" />
+        {(toggleMenu || screenWidth > 700) && (
+          <ul className={classes.menuList}>
+            <li onClick={toElement}>Blog</li>
+            <li>About</li>
+            <li>Something</li>
+            <li>Contact</li>
+            <li>Order</li>
+          </ul>
+        )}
+        {/* <Threelines onClick={toggleNav} className={classes.threeLines} /> */}
+        <button className={classes.btn} onClick={toggleNav}>
+          <Threelines className={classes.threeLines} />
+        </button>
+      </nav>
+    </>
   );
 };
 
